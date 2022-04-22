da73e0ef9c6b0d2d7a86eaa239985be55875e259
NaNnaru <63537140+kentaokamoto@users.noreply.github.com>
2022-04-23 00:12:42 +0900

    change

--- app.ino
+++ app.ino
@@ -11,6 +11,12 @@
 
 #include <Arduino_LSM9DS1.h>
 
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+
+#define LED_PIN 13
+#define MOTOR0_PIN 14
+
 rcl_publisher_t publisher_imu;
 rcl_publisher_t publisher_mag;
 
@@ -27,7 +33,12 @@ sensor_msgs__msg__Imu msg_imu;
 sensor_msgs__msg__MagneticField msg_mag;
 std_msgs__msg__Int32 msg_pwm;
 
-int motor0_pin = 14;
+void error_loop(){
+  while(1){
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+    delay(100);
+  }
+}
 
 void motor_callback(const void * msgin)
 {
@@ -72,77 +83,76 @@ void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
     msg_mag.magnetic_field.y = my;
     msg_mag.magnetic_field.z = mz;
 
-    rcl_publish(&publisher_imu, (const void *) &msg_imu, NULL);
-    rcl_publish(&publisher_mag, (const void *) &msg_mag, NULL);
+    RCSOFTCHECK(rcl_publish(&publisher_imu, (const void *) &msg_imu, NULL));
+    RCSOFTCHECK(rcl_publish(&publisher_mag, (const void *) &msg_mag, NULL));
   }
 }
 
 void setup() {
   set_microros_transports();
+  
   IMU.begin();
 
-  pinMode(motor0_pin, OUTPUT);
+  pinMode(MOTOR0_PIN, OUTPUT);
   
   delay(5000);
   analogWriteResolution(12);
-  analogWriteFrequency(motor0_pin, 250);
+  analogWriteFrequency(MOTOR0_PIN, 250);
   delay(1000);
-  analogWrite(motor0_pin, 4095);
+  analogWrite(MOTOR0_PIN, 4095);
   delay(1000);
-  analogWrite(motor0_pin, 0);
+  analogWrite(MOTOR0_PIN, 0);
   delay(1000);
   ///////////////////////////////////////////////////////////////////////////////////////////////////
   allocator = rcl_get_default_allocator();
 
   // create init_options
-  rclc_support_init(&support, 0, NULL, &allocator);
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
 
   // create node
-  rclc_node_init_default(&node, "pi_esp_communicator", "", &support);
+  RCCHECK(rclc_node_init_default(&node, "pi_esp_communicator", "", &support));
 
   // create publisher
-  rclc_publisher_init_default(
+  RCCHECK(rclc_publisher_init_default(
     &publisher_imu,
     &node,
     ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, Imu),
-    "/copto/imu");
+    "/copto/imu"));
 
-  rclc_publisher_init_default(
+  RCCHECK(rclc_publisher_init_default(
     &publisher_mag,
     &node,
     ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, MagneticField),
-    "/copto/mag");
+    "/copto/mag"));
 
 
   // create subscriber
-  rclc_subscription_init_default(
+  RCCHECK(rclc_subscription_init_default(
     &pwm_subscriber,
     &node,
     ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-    "/copto/motor0_pwm");
+    "/copto/motor0_pwm"));
 
   // create timer,
   const unsigned int timer_timeout = 100;
-  rclc_timer_init_default(
+  RCCHECK(rclc_timer_init_default(
     &timer,
     &support,
     RCL_MS_TO_NS(timer_timeout),
-    timer_callback);
+    timer_callback));
 
   // create executor
-  executor = rclc_executor_get_zero_initialized_executor();
-  rclc_executor_init(&executor, &support.context, 2, &allocator);
-  rclc_executor_add_timer(&executor, &timer);
+  RCCHECK( rclc_executor_init(&executor, &support.context, 2, &allocator));
+  RCCHECK(rclc_executor_add_timer(&executor, &timer));
 
-  motor_executor = rclc_executor_get_zero_initialized_executor();
-  rclc_executor_init(&motor_executor, &support.context, 2, &allocator);
-  rclc_executor_add_subscription(&motor_executor, &pwm_subscriber, &msg_pwm, &motor_callback, ON_NEW_DATA);
+  RCCHECK(rclc_executor_init(&motor_executor, &support.context, 2, &allocator));
+  RCCHECK(rclc_executor_add_subscription(&motor_executor, &pwm_subscriber, &msg_pwm, &motor_callback, ON_NEW_DATA));
 }
 
 void loop() {
-  rclc_executor_spin(&executor);
+  RCSOFTCHECK(rclc_executor_spin(&executor));
   delay(100);
-  rclc_executor_spin(&motor_executor);
+  RCSOFTCHECK(rclc_executor_spin(&motor_executor));
   delay(100);
   analogWrite(motor0_pin, 1000);
   delay(100);
